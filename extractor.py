c91e62300612ea7b58c9080d91d4e1a732ce2db4
niichirin <itschristaguba@gmail.com>
2022-11-06 23:44:03 -0800

    Added pyperclip copy and paste

--- extractor.py
+++ extractor.py
@@ -1,58 +1,55 @@
 import re
 import pyperclip
 
-phoneNumArray = []
-phoneNumbers = '''
-    2124567890.hahahaha
-    hahahahsada
-    212-456-7890.hahaha
-    (212)456-7hahaha890.
-    hahaha
-    (212)-456-7890.
-    212.456. 7890.
-    212 456 7890.
-    hahaha
-    +12hahaha124567890.
-    +12124567890.
-'''
+# saves whatever is on your clipboard
+# ex. if you have "spam" copied, then that is pasted into text
+text = str(pyperclip.paste())
+
 phoneRegex = re.compile(r'''
-    (\+\d)?             # 1. Country Code
-    (-|\s|\.)?          # 2. Separator
-    (\d{3}|\(\d{3}\))   # 3. Area code 323 or (323)
-    (-|\s|\.)?          # 4. Separator (grouped pipes)
-    (\d{3})             # 5. First three digits
-    (-|\s|\.)?          # 6. Separator (grouped pipes)
-    (\d{4})             # 7. Last four digits
-    (\s*(ext|x|ext.)\s*(\d{2,5}))? # Extension
+    (\+\d)?             # [0] Country Code
+    (-|\s|\.)?          # [1] Separator
+    (\d{3}|\(\d{3}\))   # [2] Area code 323 or (323)
+    (-|\s|\.)?          # [3] Separator (grouped pipes)
+    (\d{3})             # [4] First three digits
+    (-|\s|\.)?          # [5] Separator (grouped pipes)
+    (\d{4})             # [6] Last four digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))? # [7] Extension
+''', re.VERBOSE)
+emailRegex = re.compile(r'''
+    \w+@[a-zA-Z]+\.[a-zA-Z]+
 ''', re.VERBOSE)
 
-emails = '''
-peterhoeg@1me.com
-yamla@1att.net
-sbmrjbr@1gmail.com
-barlow@verizon.net
-killmenow@me.com
-dimensio@mac.com
-rattenbt@yahoo.ca
-nelson@comcast.net
-rhavyn@hotmail.com
-nicktrig@sbcglobal.net
-mcmillan@outlook.com
-mschwartz@msn.com
-'''
-emailRegex = re.compile(r'\w+@[a-zA-Z]+\.[a-zA-Z]+')
+phoneMatches = re.findall(phoneRegex, text)
+emailMatches = re.findall(emailRegex, text)
 
-phoneMatch = re.findall(phoneRegex, phoneNumbers)
-emailMatch = re.findall(emailRegex, emails)
+def findMatches():
+    matches = []
+    # Iterate through array of lists of groups
+    for groups in phoneMatches:
+        # Convert into uniform pattern
+        # Target groups with the primary digits (See phoneRegex)
+        # parameter in join must be iterable [array]
+        phoneNum = '-'.join([groups[2], groups[4], groups[6]])
+        # Insert country code
+        if groups[0] != "":
+            phoneNum = groups[1] + '-' + phoneNum
+        # Append extension
+        if groups[7] != "":
+            phoneNum += " x" + groups[8]
 
-def convertPhoneArray(array):
-    for group in array:
-        phoneNum = ''
-        for i in group:
-            phoneNum += i
-        # remove leading spaces before first character
-        phoneNumArray.append(phoneNum.lstrip())
+        matches.append(phoneNum)
+    
+    for groups in emailMatches:
+        matches.append(groups)
 
-convertPhoneArray(phoneMatch)
-print(phoneNumArray)
-print(emailMatch)
+    copyToClipboard(matches)
+
+def copyToClipboard(matches):
+    if len(matches) > 0:
+        pyperclip.copy("\n".join(matches))
+        print("Copied to Clipboard:\n")
+        print("\n".join(matches))
+    else:
+        print("No matches found")
+        
+findMatches()
